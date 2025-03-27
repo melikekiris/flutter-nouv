d02d1e21b4ee8faf8f3d1789c3665ceda0a441f2
melikekiris <melikekiris24@gmail.com>
2025-03-27 17:29:30 +0100

    Update bdd_pdo.php

--- nouveau/bdd_pdo.php
+++ nouveau/bdd_pdo.php
@@ -1,9 +1,18 @@
 <?php
-function bdd() {
-    $db = "mysql:host=localhost;dbname=equihorizon";
-    $user = "root";
-    $pass = "";
-    $con = new PDO($db, $user, $pass);
-    return $con;
+$host = 'localhost';
+$dbname = 'equihorizon'; 
+$username = 'root';         
+$password = '';             
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Activer les erreurs PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo json_encode([
+        "success" => false,
+        "message" => "Erreur de connexion à la base de données : " . $e->getMessage()
+    ]);
+    exit;
 }
 ?>
